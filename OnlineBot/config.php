d160868cfab0d44ef4844d1039468c904d277cdb
CyberSoldierUK <renato.martins39@gmail.com>
2021-10-08 18:19:01 +0100

    Online Bot
    Bot displays how many people are online on the server

---
+++ OnlineBot/config.php
@@ -0,0 +1,19 @@
+<?php
+
+/*	Server IP			*/						$ts3_ip = '';
+/*	UDP Port			*/						$ts3_port = 100;	
+/*	Query Port 			*/						$ts3_queryport = 10011;
+/*	Query login			*/						$ts3_user = '';	
+/*	Query pass			*/						$ts3_pass = '';	
+/*	Bot nickname		*/						$bot_nickname = 'OnlineBot'; 
+/*	Bot default channel	*/						$bot_move = 123;
+/*	#Sleep	"sec"		*/						$sleep = 60;	
+
+/*	Server Name		"1=On" "0=Off"  */		$Server = 1; 
+					/*	ServerName	*/			$server_name = "Online:";
+	
+/*	Channel Name	"1=On" "0=Off"  */		$Channel = 1;
+					/*	Channel name*/			$channel_name = "[cspacer]Server Online:";
+					/*	Channel ID	*/			$channel_id = 123;
+				
+?>
